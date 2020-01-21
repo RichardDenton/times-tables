b04e21c60794a3fcc819bf1ce84d9455d7d526a2
Richard Denton <richdenton@gmail.com>
2020-01-21 22:27:47 +0000

    Completed all test functions and added keyboard support.

--- script.js
+++ script.js
@@ -10,26 +10,76 @@ const buttonPress = function(e) {
                 if (testStarted) submitAnswer();
                 break;
             case 'standardtest':
+            case 'randomtest':
                 if (answerDisplayString > 0 && !testStarted) {
-                    startStandardTest(answerDisplayString);
+                    startTest(answerDisplayString, this.id);
                 }
+                break;
         }
     }
 }
 
-const startStandardTest = function(test) {
+const keyPress = function(e) {
+    if (!testFinished) {
+        if (!isNaN(e.key)) {
+            updateAnswerDisplay(e.key);
+        } else {
+            switch (e.key) {
+                case 'Backspace':
+                    del();
+                    break;
+                case 'Enter':
+                    if (testStarted) submitAnswer();
+                    break;
+                case 'S':
+                case 's':
+                    if (answerDisplayString > 0 && !testStarted) {
+                        startTest(answerDisplayString, 'standardtest');
+                    }
+                    break;
+                case 'R':
+                case 'r':
+                    if (answerDisplayString > 0 && !testStarted) {
+                        startTest(answerDisplayString, 'randomtest');
+                    }
+                    break;
+            }
+        }
+    } else if (e.key === 'Enter') location.reload();
+}
+
+const startTest = function(test, type) {
     timesTable = Number(test);
     testStarted = true;
-    for (let x=1; x <= 12; x++) {
+    for (let x=1; x <= maxFactor; x++) {
         testArray[x-1] = x;
     }
+    if (type === 'standardtest') {
+        const standardTestButton = document.getElementById('standardtest');
+        standardTestButton.classList.add('selectedtest');
+    } else {
+        const randomTestButton = document.getElementById('randomtest');
+        randomTestButton.classList.add('selectedtest');
+        shuffleTest();
+    }
     promptQuestion(testArray.shift(), timesTable);
+}
+
+const shuffleTest = function() {
+    for (let x = testArray.length - 1; x >= 0; x--) {
+        const y = Math.floor(Math.random() * x);
+        const newX = testArray[y];
+        const newY = testArray[x];
+        testArray[x] = newX;
+        testArray[y] = newY;
+        }
 }
 
 const promptQuestion = function(factor1, factor2) {
     questionDisplay.textContent = factor1 + " × " + factor2 + " =";
     currentQuestion = [factor1, factor2];
     clearAnswerDisplay();
+    updateScoreDisplay();
 }
 
 const updateAnswerDisplay = function(character) {
@@ -39,7 +89,7 @@ const updateAnswerDisplay = function(character) {
         answerDisplayString = character;
     }
     if (!testStarted) questionDisplay.textContent = "Press standard or random to begin";
-    if (answerDisplayString > 12 && !testStarted) answerDisplayString = "12";
+    if (answerDisplayString > maxFactor && !testStarted) answerDisplayString = `${maxFactor}`;
     answerDisplay.textContent = answerDisplayString;
 }
 
@@ -63,7 +113,18 @@ const submitAnswer = function() {
         mark = '✅';
     }
     const resultText = document.getElementById(`q${questionNumber}result`);
-    resultText.textContent = `${currentQuestion[0]} × ${currentQuestion[1]} = ${answerDisplayString} ${mark} ${correctAnswer}`;
+    resultText.textContent = `${questionNumber}: ${currentQuestion[0]} × ${currentQuestion[1]} = ${answerDisplayString} ${mark} ${correctAnswer}`;
+    updateScoreDisplay();
+    questionNumber += 1;
+    if (questionNumber <= maxFactor) {
+        promptQuestion(testArray.shift(), timesTable);
+    } else {
+        endTest();
+    }
+}
+
+const updateScoreDisplay = function() {
+    scroreDisplay.textContent = `Score: ${score} / ${maxFactor}`;
 }
 
 const clearAnswerDisplay = function() {
@@ -71,16 +132,32 @@ const clearAnswerDisplay = function() {
     answerDisplay.textContent = answerDisplayString;
 }
 
-let testArray = []; // Store 1 - 12 to allow tests to be randomised
+const endTest = function() {
+    questionDisplay.textContent = "Press submit to start again";
+    answerDisplayString = '0';
+    updateAnswerDisplay(`Final score: ${score} / ${maxFactor}`);
+    buttons.forEach(button => button.removeEventListener('click', buttonPress));
+    const submitButton = document.getElementById('submit');
+    submitButton.addEventListener('click', () => location.reload());
+    testFinished = true;
+}
+
+const maxFactor = 12; // Highest timestable allowed
+
+let testArray = []; 
 let currentQuestion = []; 
 
 let timesTable = 0;
 let score = 0;
 let questionNumber = 1;
 let testStarted = false;
+let testFinished = false;
 
+const scroreDisplay = document.getElementById('score');
 const questionDisplay = document.getElementById("questions");
+questionDisplay.textContent = `Enter which test to take (1-${maxFactor}):`
 const answerDisplay = document.getElementById("answer");
 let answerDisplayString = "";
 const buttons = document.querySelectorAll("button");
 buttons.forEach(button => button.addEventListener('click', buttonPress));
+window.addEventListener('keydown', keyPress);
